6b795165a0d120d7bf3d746de08d471abdce8c30
Liqiankun <qiankunli@yahoo.com>
2017-07-08 23:09:32 +0800

    First Commit

---
+++ LearningExpress/meadowlark.js
@@ -0,0 +1,22 @@
+var express = require('express')
+
+var app = express()
+
+app.set('port', process.env.PORT || 3000)
+
+app.use(function(req, res){
+  res.type('text/plain')
+  res.status(404)
+  res.send('404 - Not Found')
+})
+
+app.use(function(err, req, res, next){
+  console.error(err.stack)
+  res.type('text/plain')
+  res.status(500)
+  res.send('500 - Server Error')
+})
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Crtl-C to terminate.');
+})
