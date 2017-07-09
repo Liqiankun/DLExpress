8a84739c1a3a7fb9b5d620fe06c779b4b36c9780
Liqiankun <qiankunli@yahoo.com>
2017-07-09 20:08:14 +0800

    Tests

--- LearningExpress/meadowlark.js
+++ LearningExpress/meadowlark.js
@@ -2,10 +2,21 @@ var express = require('express')
 var app = express()
 var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' })
 var fortune = require('./lib/fortune.js')
+var tours = [
+  {id: 0, name: 'Hood River', price: 99.9 },
+  {id: 1, name: 'Oregon Coast', price: 149.95 }
+]
 
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 app.set('port', process.env.PORT || 3000)
+
+if(app.thing == null) console.log( 'bleat' )
+
+app.use(function(req, res, next){
+  res.locals.showTests = app.get('env' !== 'production' && req.query.test === '1')
+  next()
+})
 
 app.use(express.static(__dirname + '/public'))
 
@@ -14,7 +25,23 @@ app.get('/', function(req, res){
 })
 
 app.get('/about', function(req, res){
-  res.render('about', { fortune: fortune.getFortune() })
+  res.render('about', { fortune: fortune.getFortune(), pageTestScript: '/qa/tests-about.js' })
+})
+
+app.get('/tours/hood-river', function(req, res){
+  res.render('tours/hood-river')
+})
+
+app.get('/tours/request-group-rate', function(req, res){
+  res.render('tours/request-group-rate')
+})
+
+app.get('/api/tours', function(req, res){
+  res.format({
+    'application/json': function(){
+      res.json(tours)
+    }
+  })
 })
 
 app.use(function(req, res){
---
+++ LearningExpress/public/qa/tests-crosspage.js
@@ -0,0 +1,36 @@
+var Browser = require('zombie')
+var assert = require('chai').assert
+var browser
+
+suite('Cross-Page Tests', function(){
+  setup(function(){
+    browser = new Browser()
+  })
+
+  test('requesting a group rate quote from the hood river tour page' + 'should populate the referrer field', function(done){
+    var referrer = 'http://localhost:3000/tours/hood-river'
+    browser.visit(referrer, function(){
+      browser.clickLink('.requestGroupRate', function(){
+        assert(browser.field('referrer').value ===  referrer)
+        done()
+      })
+    })
+  })
+
+  test('requesting a group rate from the oregon coast tour page should' + 'pipulate th referrer field', function(done){
+    var referrer = 'http://localhost:3000/tours/oregan-coast'
+    browser.visit(referrer, function(){
+      browser.clickLink('.requestGroupRate', function(){
+        assert(browser.field('referrer').value === referrer)
+        done()
+      })
+    })
+  })
+
+  test('visiting the "request group rate" page dirctly should be result' + 'in an empty referrer field', function(done){
+    browser.visit('http://localhost:3000/tours/request-group-rate', function(){
+      assert(browser.field('referrer').value === '')
+      done()
+    })
+  })
+})
