1c9d62a2f393b5b92316fc867c766225ecfea85f
Liqiankun <qiankunli@yahoo.com>
2017-07-09 10:01:45 +0800

    Fortune

--- LearningExpress/meadowlark.js
+++ LearningExpress/meadowlark.js
@@ -1,20 +1,36 @@
 var express = require('express')
-
 var app = express()
+var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' })
 
+var fortunes = [
+  'Conquer your fears or they will conquer you.',
+  'River need spring.',
+  'Do not fear what you do not know',
+  'You will have a plesant surprise',
+  'Whenever possible, keep it simple'
+]
+
+app.engine('handlebars', handlebars.engine)
+app.set('view engine', 'handlebars')
 app.set('port', process.env.PORT || 3000)
 
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(req, res){
+  res.render('home')
+})
+
+app.get('/about', function(req, res){
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+  res.render('about', { fortune: randomFortune })
+})
+
 app.use(function(req, res){
-  res.type('text/plain')
-  res.status(404)
-  res.send('404 - Not Found')
+  res.render('404')
 })
 
 app.use(function(err, req, res, next){
-  console.error(err.stack)
-  res.type('text/plain')
-  res.status(500)
-  res.send('500 - Server Error')
+  res.render('500')
 })
 
 app.listen(app.get('port'), function(){
