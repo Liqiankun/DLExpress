39c1ef0328fea82ce216e32ee7e8014421b4c77c
Liqiankun <qiankunli@yahoo.com>
2017-07-21 22:34:11 +0800

    use routes

--- LearningExpress/index.js
+++ LearningExpress/index.js
@@ -1,10 +1,15 @@
 var express = require('express')
+var routes = require('./routes/api')
 /* set up express app */
 var app = express()
+
 /* listen fro request */
 app.listen(process.env.port || 4000, function () {
   console.log('express begin listening request')
 })
+
+/* use router */
+app.use('/api', routes)
 
 app.get('/', function (req, res) {
   res.send('GET request')
---
+++ LearningExpress/routes/api.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var router = express.Router()
+
+/* get the list of books */
+router.get('/books', function (req, res) {
+  res.send({ name: 'GET' })
+})
+
+/* create a new book */
+router.post('/books', function (req, res) {
+  res.send({ name: 'POST' })
+})
+
+/* update book */
+router.put('/books/:id', function (req, res) {
+  res.send({ name: 'PUT' })
+})
+
+/* delete book */
+router.delete('/books/:id', function (req, res) {
+  res.send({ name: 'DELETE' })
+})
+
+module.exports = router
